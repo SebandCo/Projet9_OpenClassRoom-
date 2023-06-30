67fae0b033dec3045abb72c5fe2f90d93b3379d9
SebandCo <113468072+SebandCo@users.noreply.github.com>
2023-06-30 22:54:55 +0200

    Revert "Revert "travail sur les permissions""
    This reverts commit d6113cd9e9bc8ef528b3e7c50457fe14477782a7.

--- HTML_Python/blog/views.py
+++ HTML_Python/blog/views.py
@@ -60,40 +60,6 @@ def creation_ticket(request):
                   context = {"ticket_form": ticket_form})
 
 
-'''@login_required
-def creation_ticket(request):
-    ticket_form = forms.TicketForm()
-    critique_form = forms.TicketEtCritiqueForm()
-    if request.method == "POST":
-        ticket_form= forms.TicketForm(request.POST, request.FILES)
-        critique_form= forms.TicketEtCritiqueForm(request.POST)
-        # On vérifie que le formulaire est valide
-        if ticket_form.is_valid():
-            ticket = ticket_form.save(commit=False)
-            ticket.auteur = request.user
-            ticket.save()
-            # Augmente de 1 le nombre de ticket posté par l'utilisateur
-            request.user.nombre_ticket += 1
-            request.user.save()
-            print(critique_form)
-            if critique_form.is_valid():
-                critique = critique_form.save(commit=False) 
-                critique.auteur = request.user
-                print(ticket.id)
-                critique.ticket = ticket
-                critique.save()
-                # Augmente de 1 le nombre de critique cumulé pour le titre
-                critique.ticket.nombre_critique += 1
-                critique.ticket.save()
-                # Augmente de 1 le nombre de critique posté par l'utilisateur
-                request.user.nombre_critique += 1
-                request.user.save()
-            return redirect("home")
-    return render(request,
-                  "blog/creation_ticket.html",
-                  context = {"ticket_form": ticket_form,
-                             "critique_form": critique_form})'''
-
 @login_required
 def affichage_des_tickets(request):
     tickets = models.Ticket.objects.all()
